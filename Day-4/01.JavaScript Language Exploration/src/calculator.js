03017996449727c891dbcd05f43b4f27c21e88f3
tkmagesh <tkmagesh77@gmail.com>
2014-03-13 12:27:16 +0530

    Closure Assignment

--- Day-4/01.JavaScript Language Exploration/src/calculator.js
+++ Day-4/01.JavaScript Language Exploration/src/calculator.js
@@ -5,16 +5,5 @@ function sum(x,y){
 		if (isNaN(n)) return 0;
 		return parseInt(n);
 	}
-	/*function getRemaining(args){
-		var result = [];
-		for(var i=1;i<args.length;i++)
-			result.push(args[i]);
-		return result;
-	}*/
-	return arguments.length <= 1 ? parseArg(arguments[0]) 
-		: parseArg(arguments[0]) + sum.apply(this,[].slice.call(arguments,1));
-	/*var result = 0;
-	for(var i=0;i<arguments.length;i++)
-		result += parseArg(arguments[i]);
-	return result;*/
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + sum.apply(this,[].slice.call(arguments,1));
 }
